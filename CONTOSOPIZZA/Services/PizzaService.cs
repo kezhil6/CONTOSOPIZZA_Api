6592a08665649cd43440ae98709854fd1ae51f8d
Ezhil <kezhil6@gmail.com>
2026-03-06 00:21:02 +0530

    Add project files.

---
+++ CONTOSOPIZZA/Services/PizzaService.cs
@@ -0,0 +1,48 @@
+﻿using CONTOSOPIZZA.Models;
+
+namespace CONTOSOPIZZA.Services
+{
+    public class PizzaService
+    {
+        static List<Pizza> Pizzas { get; set;}
+        static int nextId = 3;
+        static PizzaService()
+        {
+            Pizzas =     new List<Pizza>
+            {
+                new Pizza { Id = 1, Name = "CheezePizza", IsGlutenFree = true },
+                new Pizza { Id = 2, Name = "normal", IsGlutenFree = false }
+            };
+        }
+
+        public static List<Pizza> GetAll() => Pizzas;
+
+        public static Pizza? Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
+
+        public static void Add(Pizza pizza)
+        {
+            pizza.Id = nextId++;
+            Pizzas.Add(pizza);
+        }
+
+        public static void Delete(int id)
+        {
+            var pizza = Get(id);
+            if (pizza is null) {
+                return;
+            }
+
+            Pizzas.Remove(pizza);
+        }
+
+        public static void Update(Pizza pizza)
+        {
+            var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+            if(index == -1) {
+                return;
+            }
+
+            Pizzas[index] = pizza;
+        }
+    } 
+}
